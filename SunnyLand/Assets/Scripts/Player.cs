7eb31a6428e1b7a6981584cdf9d50c373572dcdd
fdsosistema <ryu.rom.kos@gmail.com>
2025-06-11 09:20:52 -0300

    aaa

---
+++ SunnyLand/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string nome = "DBD";
+    public int HP = 5;
+    public float speed = 2;
+    private bool isAlive = true;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        Debug.Log("Nome:" + nome + "\nVida:" + HP + "\nVelocidade:" + speed + "\nVivo ou morto:" + isAlive);
+    rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
